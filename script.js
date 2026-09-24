b8dda147f823bbd138f7d09d922578cd2b6aea08
ItsNimue <nobodynimue@gmail.com>
2026-09-24 21:29:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://pairbackend-production-6f0f.up.railway.app";
+const API_BASE_URL = "https://backend-production-5f64.up.railway.app";
 
 const btnCode = document.getElementById("btn-code");
 const phoneInput = document.getElementById("phone");
